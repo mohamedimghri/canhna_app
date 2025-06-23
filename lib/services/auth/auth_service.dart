761c62817d87d3bd8dfee6add2353b4df2a238b8
Mohamed Imghri <mohamedimghri06@gmail.com>
2025-06-23 01:14:55 +0100

    authService login , signup, logout...

---
+++ lib/services/auth/auth_service.dart
@@ -0,0 +1,81 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class AuthService {
+  final SupabaseClient _supabase = Supabase.instance.client;
+
+  // Sign in
+  Future<AuthResponse> signInWithEmailPassword(
+    String email,
+    String password,
+  ) async {
+    return await _supabase.auth.signInWithPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  // Sign up
+  Future<AuthResponse> signUpWithEmailPassword(
+    String email,
+    String password,
+    String name,
+    String role,
+  ) async {
+    final response = await _supabase.auth.signUp(
+      email: email,
+      password: password,
+    );
+
+    final user = response.user;
+
+    if (user != null) {
+      // print('User ID: ${user.id}');
+      try {
+        await _supabase.from('profiles').insert({
+          'id': user.id,
+          'name': name,
+          'role': role,
+          'is_active': true
+        });
+      } catch (e) {
+        print('Insert error: $e');
+      }
+    }
+
+    return response;
+  }
+
+  // Sign out
+  Future<void> signOut() async {
+    await _supabase.auth.signOut();
+  }
+
+  // Get current user email
+  String? getCurrentUserEmail() {
+    final session = _supabase.auth.currentSession;
+    final user = session?.user;
+    return user?.email;
+  }
+
+  // Get current user ID
+  String? getCurrentUserId() {
+    return _supabase.auth.currentUser?.id;
+  }
+
+  
+
+  // Get user role from profiles table
+  Future<String?> fetchUserRole(String userId) async {
+    final response =
+        await _supabase
+            .from('profiles') // <- updated table name
+            .select('role')
+            .eq(
+              'id',
+              userId,
+            ) // <- should match 'id' in profiles (same as auth.users.id)
+            .single();
+
+    return response['role'];
+  }
+}
