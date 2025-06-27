adece7f0a248e428f3aa56197d5e4fe184f6737f
Abdelali Duiche <abdodui70@gmail.com>
2025-06-27 18:48:29 +0100

    Add admin needed screens.

---
+++ lib/views/admin/profile_admin.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProfileAdmin extends StatefulWidget {
+  const ProfileAdmin({super.key});
+
+  @override
+  State<ProfileAdmin> createState() => _ProfileAdminState();
+}
+
+class _ProfileAdminState extends State<ProfileAdmin> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
