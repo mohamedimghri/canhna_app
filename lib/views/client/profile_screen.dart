91d6b1e6fd60878c24c477e609662ca95cf602a6
Mohamed Imghri <mohamedimghri06@gmail.com>
2025-06-27 13:43:19 +0100

    add logout button

--- lib/views/client/profile_screen.dart
+++ lib/views/client/profile_screen.dart
@@ -1,3 +1,5 @@
+import 'package:canhna_app/services/auth/auth_service.dart';
+import 'package:canhna_app/views/auth/Login_screen.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatefulWidget {
@@ -8,11 +10,24 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  final aut = AuthService();
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: Text("Profil"),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              await aut.signOut();
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => LoginScreen()),
+              );
+            },
+            icon: Icon(Icons.logout),
+          ),
+        ],
       ),
     );
   }
